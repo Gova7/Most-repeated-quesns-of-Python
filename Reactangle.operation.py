a389a46a3f869f91ba5a5f1097c4531c6476151b
Govardhan Guduri <87258106+Gova7@users.noreply.github.com>
2021-09-21 14:12:19 +0530

    Add files via upload

---
+++ Reactangle.operation.py
@@ -0,0 +1,55 @@
+# Question 14 - Define a class Rectangle. The class should contain sides: length and breadth of the rectangle as data members. It should report the following methods
+
+# (a) - __init__ for initializing data members length and breadth
+# (b) - setLength() for updating the length of the rectangle
+# (c) - setBreadth() for updating the breadth of the rectangle
+# (d) - getLength() for retrieving the length of the rectangle
+# (e) - getBreadth() for retrieving the breadth of the rectangle
+# (f) - area() to find the area of the rectangle
+# (g) - perimeter() to find the perimeter of the rectangle
+
+
+class Rectangle:
+  length = 0
+  breadth = 0
+
+  def __init__(self, len, bre):
+    self.length = len
+    self.breadth = bre
+
+  def setLength(self, newLength):
+    self.length = newLength
+
+  def setBreadth(self, newBreadth):
+    self.breadth = newBreadth
+
+  def getLength(self):
+    return self.length
+
+  def getBreadth(self):
+    return self.breadth
+
+  def area(self):
+    return self.length * self.breadth
+
+  def perimeter(self):
+    return 2 * (self.length + self.breadth)
+
+
+rect = Rectangle(10, 20)
+
+print("Rectangle dimensions: ")
+print(rect.length, rect.breadth)
+
+print("Updating rectangle dimensions: ")
+rect.setLength(20)
+rect.setBreadth(40)
+
+print("New dimensions: ")
+print(rect.getLength(), rect.getBreadth())
+
+print("Area of rectangle: ")
+print(rect.area())
+
+print("Perimeter of rectangle: ")
+print(rect.perimeter())
---
+++ create-a-class.py
@@ -0,0 +1,22 @@
+# Create a class Student having data members rollno, name, stream and marksList in which is a list of marks in 5 subjects
+# use __init__() function to initialise data members.
+# Make data member avg to calculate avg of marks of student
+
+class Student:
+  def __init__(self, name, roll, stream, marks):
+    self.name = name
+    self.rollno = roll
+    self.stream = stream
+    self.marksList = marks
+
+  def avg(self):
+    return sum(self.marksList) / 3
+
+
+student = Student("Shaan Alam", 27, "BSC APS", [98, 99, 100])
+
+print("Name = ", student.name)
+print("Roll no = ", student.rollno)
+print("Stream = ", student.rollno)
+print("Marks = ", student.marksList)
+print("Average marks of", student.name, " = ", student.avg())
